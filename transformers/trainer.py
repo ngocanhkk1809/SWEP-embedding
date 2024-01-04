22ef2d6a3e86cd26692061de4e0bda4a76fd9a37
dat <dat.nc200137@sis.hust.edu.vn>
2024-01-05 03:04:12 +0700

    init commit

--- transformers/trainer.py
+++ transformers/trainer.py
@@ -15,7 +15,7 @@
 """
 The Trainer class, to easily train a 🤗 Transformers from scratch or finetune it on a new task.
 """
-
+import wandb
 import contextlib
 import copy
 import functools
@@ -3046,11 +3046,10 @@ class Trainer:
                 metrics.update(dataset_metrics)
             return metrics
 
-        # memory metrics - must set up as early as possible
-        self._memory_tracker.start()
+        # # memory metrics - must set up as early as possible
+        # self._memory_tracker.start()
 
         eval_dataloader = self.get_eval_dataloader(eval_dataset)
-        start_time = time.time()
 
         eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
         output = eval_loop(
@@ -3063,27 +3062,8 @@ class Trainer:
             metric_key_prefix=metric_key_prefix,
         )
 
-        total_batch_size = self.args.eval_batch_size * self.args.world_size
-        if f"{metric_key_prefix}_jit_compilation_time" in output.metrics:
-            start_time += output.metrics[f"{metric_key_prefix}_jit_compilation_time"]
-        output.metrics.update(
-            speed_metrics(
-                metric_key_prefix,
-                start_time,
-                num_samples=output.num_samples,
-                num_steps=math.ceil(output.num_samples / total_batch_size),
-            )
-        )
         if step is not None:
-            self.log(output.metrics, step)
-
-        if DebugOption.TPU_METRICS_DEBUG in self.args.debug:
-            # tpu-comment: Logging debug metrics for PyTorch/XLA (compile, execute times, ops, etc.)
-            xm.master_print(met.metrics_report())
-
-        self.control = self.callback_handler.on_evaluate(self.args, self.state, self.control, output.metrics)
-
-        self._memory_tracker.stop_and_update_metrics(output.metrics)
+            wandb.log(output.metrics, step=step)
 
         return output.metrics
 
